74ebcc60cfa976e158333c827f2b5379ec9a7836
LastW1 <przbog123@wp.pl>
2022-05-08 18:19:05 +0200

    gathering information about final step in search image

--- BotHelper/BotHelper/PictureManager.cs
+++ BotHelper/BotHelper/PictureManager.cs
@@ -18,13 +18,20 @@ namespace BotHelper
             var bmp = new Bitmap((int)width, (int)height);
             Graphics g = Graphics.FromImage(bmp);
             g.CopyFromScreen(0, 0, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
-            bmp.Save("test.png", ImageFormat.Png); // png jest bezstratnym zapisem, nie tworzy artefaktów
+          //  bmp.Save("test.png", ImageFormat.Png); // png jest bezstratnym zapisem, nie tworzy artefaktów
 
             var lookForBmp = new Bitmap("LookFor.png"); // bmp szukanego wzorca
 
+            var matchPixelCountNeeded = (lookForBmp.Height * lookForBmp.Width) * 0.7;
+
             var findIt = false;
-            for (var i = 0; i < bmp.Width - lookForBmp.Width; i+=10)
-                for (var j = 0; j < bmp.Height - lookForBmp.Height; j+=10)
+            var widthStepSize = (int)(lookForBmp.Width * 0.09);
+            var heighStepSize = (int)(lookForBmp.Height * 0.09);
+            var stepCounter = 0;
+            int? findX = null;
+            int? findY = null;
+            for (var i = 0; i < bmp.Width - lookForBmp.Width; i+= widthStepSize)
+                for (var j = 0; j < bmp.Height - lookForBmp.Height; j+= heighStepSize)
                 {
                     if (findIt)
                     {
@@ -43,20 +50,24 @@ namespace BotHelper
                                 break;
                             }
 
+                            stepCounter++;
                             if (subBmp.GetPixel(w, h) == lookForBmp.GetPixel(w, h))
                             {
                                 matchCounter++;
                             }
                         }
 
-                    if (matchCounter > (lookForBmp.Height * lookForBmp.Width) * 0.7)
+                    if (matchCounter > matchPixelCountNeeded)
                     {
+                        findX = i;
+                        findY = j;
                         findIt = true;
                     }
                 }
 
             if (findIt)
             {
+                var lol = stepCounter;
                 MessageBox.Show("Got It!");
             }
             else
